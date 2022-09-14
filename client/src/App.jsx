c22d0caddf91c4ada85a898c8205533a8e7b47bb
pankajsharma0308 <pankaj.s.0308@gmail.com>
2022-09-14 10:28:53 +0530

    navbar and Welcome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,13 @@ import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
 const App = () => {
   return (
-  <div className="min-h-screen">
+  <div className="min-h-screen" class="hello">
     <div className="gradient-bg-welcome">
       <Navbar />
       <Welcome />
     </div>
     <Services />
+    <Transactions />
     <Footer />
   </div>
 );
