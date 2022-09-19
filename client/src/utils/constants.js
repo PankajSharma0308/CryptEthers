a9f23b70142798bd4c569ef0697cc2f511004c83
pankajsharma0308 <pankaj.s.0308@gmail.com>
2022-09-19 15:53:04 +0530

    Updated Smart Contract

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x9363b072b701a901Dc281eF8f4A9240eb3D7C8e8';
--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/XzfkHma3vZShgJOhgSsJ9HzvPqEoOHDA
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.17",
-};
+    solidity: '0.8.0',
+    networks: {
+        goerli: {
+            url: 'https://eth-goerli.g.alchemy.com/v2/XzfkHma3vZShgJOhgSsJ9HzvPqEoOHDA',
+            accounts: [`b82ba2ed9ae05224ed43ba979e94a25dfb3ea853401a6660fb9f9fecf4a9f0f2`]
+
+        }
+    }
+}
