77eaffd3386c39c4c8e575fb2d8ff8e10391686e
pankajsharma0308 <pankaj.s.0308@gmail.com>
2022-10-07 23:54:28 +0530

    Updated Files

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,10 @@
+import React, {useContext} from 'react';
 import { AiFillAlipayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from "./";
-
+import { TransactionContext  } from "../context/TransactionContext";
+import { shortenAddress } from '../utils/shortenAddress';
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white';
 
@@ -19,11 +21,17 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
 
 const Welcome = () => {
 
-    const connectWallet = () => {
+    const {connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction} = useContext(TransactionContext);
 
-    }
+    const handleSubmit =(e) => {
 
-    const handleSubmit =() => {
+        const { addressTo, amount, keyword, message } = formData;
+
+        e.preventDefault();
+
+        if(!addressTo || !amount || !keyword || !message) return;
+        
+        sendTransaction(); 
 
     }
 
@@ -37,15 +45,20 @@ const Welcome = () => {
                 <p className="text-white text-1xl py-1 mt-5 text-left font-light text-base">
                     Explore the Crypto World. Buy and Sell Cryptocurrencies easily.
                 </p>   
-                <button
-                    type="button"
-                    onClick={connectWallet}
-                    className="flex flex-row justify-center item-center my-5 bg-[#2952e3] p-3 rounded-full 
-                                cursor-pointer hover:bg-[#9783ab]">
-                    <p className="text-white text-base font-semibold">
-                        Connect Wallet  
-                    </p>
-                </button>
+                {!currentAccount && 
+                    (
+                        <button
+                            type="button"
+                            onClick={connectWallet}
+                            className="flex flex-row justify-center item-center my-5 bg-[#2952e3] p-3 rounded-full 
+                                        cursor-pointer hover:bg-[#9783ab]">
+                            <p className="text-white text-base font-semibold">
+                                Connect Wallet  
+                            </p>
+                        </button>
+                    )
+                
+                }
 
                 <div className="grid sm:grid-cols-3 grid-cols-2 mt-10 w-full">
                     <div className={`rounded-tl-2xl ${commonStyles}`}>
@@ -85,7 +98,7 @@ const Welcome = () => {
                         </div>
                         <div>
                             <p className="text-white font-light text-sm ">
-                                Address
+                               { shortenAddress(currentAccount) }
                             </p>
                             <p className="text-white font-semibold text-lg mt-1 ">
                                 Ethereum
@@ -94,10 +107,10 @@ const Welcome = () => {
                     </div>
                 </div>
                 <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={()=>{} }/>
-                    <Input placeholder="Amount (ETH) " name="amount" type="number" handleChange={()=>{} }/>
-                    <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={()=>{} }/>
-                    <Input placeholder="Enter Message" name="message" type="text" handleChange={()=>{} }/>
+                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={ handleChange }/>
+                    <Input placeholder="Amount (ETH) " name="amount" type="number" handleChange={ handleChange }/>
+                    <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={ handleChange }/>
+                    <Input placeholder="Enter Message" name="message" type="text" handleChange={ handleChange }/>
                 
                     <div className="h-[1px] w-full bg-gray-400 my-2"/>
 
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,6 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   mode: "jit",
-  darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       display: ["Open Sans", "sans-serif"],
